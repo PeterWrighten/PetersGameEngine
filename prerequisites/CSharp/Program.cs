b6c921329ac18195ebb65c60f8dff4b1c005435a
PeterWrght <peterwrighten@gmail.com>
2022-04-01 07:05:37 +0900

    update::{gitignore,  csharp}

---
+++ prerequisites/CSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CSharp {
+    class Program {
+        static void Main(string[] args) {
+            int x = 2001;
+            System.Console.WriteLine(x);
+        }
+    }
+}
